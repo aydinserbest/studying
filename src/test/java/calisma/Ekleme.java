7e085921f0a61810da37e8c58133d141c4f0d77e
aydinserbest <aydinserbest34@.com>
2022-06-04 04:39:16 +0200

    ekleme yapildi

--- src/test/java/calisma/Ekleme.java
+++ src/test/java/calisma/Ekleme.java
@@ -3,5 +3,6 @@ package calisma;
 public class Ekleme {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("develop branchtan mrb");
     }
 }
